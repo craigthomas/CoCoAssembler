d2aca55fcc6763951e224131c08b97635513ed1c
Craig Thomas <craig.thomas@gmail.com>
2016-01-24 21:37:08 -0800

    Changes to regex and License file.

--- coco3asm.py
+++ coco3asm.py
@@ -1,9 +1,9 @@
-'''
-Copyright (C) 2012 Craig Thomas
+"""
+Copyright (C) 2012-2015 Craig Thomas
 This project uses an MIT style license - see LICENSE for details.
 
 A Color Computer 3 assembler - see the README.md file for details.
-'''
+"""
 # I M P O R T S ###############################################################
 
 import argparse
@@ -22,7 +22,7 @@ EXT = "Extended"
 # Dictionary entries
 LABEL = "label"
 OP = "op"
-OPERAND = "operand"
+OPERANDS = "operands"
 COMMENT = "comment"
 MODE = "mode"
 OPCODE = "opcode"
@@ -48,7 +48,7 @@ FCB = "FCB"
 FDB = "FDB"
 FCC = "FCC"
 INCLUDE = "INCLUDE"
-PSEUDO_OPERATIONS = [ END, ORG, EQU, SET, RMB, FCB, FDB, FCC, INCLUDE ]
+PSEUDO_OPERATIONS = [END, ORG, EQU, SET, RMB, FCB, FDB, FCC, INCLUDE]
 
 # Opcode operand requirements based on addressing modes
 OPERAND_COUNTS = {
@@ -58,13 +58,102 @@ OPERAND_COUNTS = {
 }
 
 # Pattern to parse a single line
-ASM_LINE_REGEX = re.compile("(?P<label>\w*)\s+(?P<op>\w*)\s+"
-    "(?P<operand>[\w#\$,\+-]*)\s+(?P<comment>.*)")
+ASM_LINE_REGEX = re.compile("(?P<" + LABEL + ">\w*)\s+(?P<" + OP + ">\w*)\s+"
+    "(?P<" + OPERANDS + ">[\w#\$,\+-]*)\s+(?P<" + COMMENT + ">.*)")
 
 # G L O B A L S ###############################################################
 
 # Stores each of the symbols and their values
 symbol_table = dict()
+
+# C L A S S E S  ##############################################################
+
+
+class TranslationError(Exception):
+    """
+    Translation errors occur when the translate function is called from
+    within the Statement class. Translation errors usually refer to the fact
+    that an invalid mnemonic or invalid register was specified.
+    """
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class Statement:
+    """
+    The Statement class represents a single line of assembly language. Each
+    statement is constructed from a single line that has the following format:
+
+        LABEL   MNEMONIC   OPERANDS    COMMENT
+
+    The statement can be parsed and translated to its COCO machine code
+    equivalent.
+    """
+    def __init__(self):
+        self.label = ""
+        self.opcode = ""
+        self.comments = ""
+        self.size = 0
+        self.address = 0
+        self.op = ""
+
+    def __str__(self):
+        return "{} {} {} {} {}  # {}".format(
+                self.address[2:].upper().rjust(4, '0'),
+                self.opcode.upper().rjust(4, '0'),
+                self.label.rjust(15, ' '),
+                self.op.rjust(5, ' '),
+                self.operands.rjust(15, ' '),
+                self.comment.ljust(40, ' '))
+
+    def parse_line(self, line):
+        """
+        Parse a line of assembly language text.
+        """
+        data = ASM_LINE_REGEX.match(line)
+        if data:
+            self.label = data.group(LABEL)
+            self.op = data.group(OP)
+            self.operands = data.group(OPERANDS)
+            self.comment = data.group(COMMENT)
+
+    def translate(self):
+        """
+        Translate the text into an actual opcode.
+        """
+        if self.op in PSEUDO_OPERATIONS:
+            return
+
+        if self.op not in OPERATIONS:
+            error = "Invalid mnemonic '{}'".format(self.op)
+            raise TranslationError(error)
+
+        operation = self.get_operation()
+        self.opcode = operation[OP]
+        if self.operands:
+            operands = self.operands.split(",")
+
+            if len(operands) != operation[OPERANDS]:
+                error = "Expected {} operand(s), but got {}".format(
+                    operation[OPERANDS], len(operands))
+                raise TranslationError(error)
+
+            counter = 0
+            for operand_type in [SOURCE, TARGET, NUMERIC]:
+                if operation[operand_type] != 0:
+                    self.set_value(operand_type, operands[counter])
+                    counter += 1
+
+    def get_operation(self):
+        """
+        Returns the operation dictionary based upon the mnemonic.
+        """
+        if self.is_pseudo_op():
+            return self.op
+        return OPERATIONS[self.op]
 
 # F U N C T I O N S ###########################################################
 
@@ -73,12 +162,31 @@ def parse_arguments():
     Parses the command-line arguments passed to the assembler.
     '''
     parser = argparse.ArgumentParser(description = "Assemble or disassmble "
-        "machine language code for the Color Computer 3. See README.md for "
-        "more information, and LICENSE for terms of use.")
+        "machine language code for the COCO3. See README.md for more "
+        "information, and LICENSE for terms of use.")
     parser.add_argument("filename", help = "the name of the file to examine")
-    parser.add_argument("-s", action = "store_true", help = "print out the " 
+    parser.add_argument("-s", action = "store_true", help = "print out the "
         "symbol table")
+    parser.add_argument("-p", action = "store_true", help = "print out the "
+        "assembled statements when finished")
+    parser.add_argument("-o", metavar = "FILE", help = "stores the assembled "
+        "program in FILE")
     return parser.parse_args()
+
+
+def throw_error(error, statement):
+    """
+    Prints out an error message.
+
+    @param error: the error message to throw
+    @type error: Exception
+
+    @param statement: the assembly statement that caused the error
+    @type statement: Statement
+    """
+    print(error["value"])
+    print("Line: " + str(statement))
+    sys.exit(1)
 
 
 def read_file(filename):
@@ -120,7 +228,7 @@ def parse_line(line):
     if data:
         result[LABEL] = data.group(LABEL)
         result[OP] = data.group(OP)
-        result[OPERAND] = data.group(OPERAND)
+        result[OPERANDS] = data.group(OPERANDS)
         result[COMMENT] = data.group(COMMENT)
     print(result)
     return result 
@@ -136,7 +244,7 @@ def translate_addressing_mode(statement):
     @return: the addressing mode associated with the operand
     @rtype: [ Inherent | Immediate | Direct | Indexed | Extended ]
     '''
-    operand = statement[OPERAND]
+    operand = statement[OPERANDS]
     if operand == "":
         return INH
     if operand.startswith("#"):
@@ -169,14 +277,14 @@ def translate_opcode(statement):
 
     if operation not in OPERATIONS:
         print("{}  {}  {}  {}".format(statement[LABEL], statement[OP],
-           statement[OPERAND], statement[COMMENT]))
+           statement[OPERANDS], statement[COMMENT]))
         print("Error: invalid mnemonic '{}'".format(statement[OP]))
         sys.exit(1)
     
     if mode not in OPERATIONS[operation]:
         print("{}  {}  {}  {}".format(statement[LABEL], statement[OP],
-           statement[OPERAND], statement[COMMENT]))
-        print("Error: syntax error in operand '{}'".format(statement[OPERAND]))
+           statement[OPERANDS], statement[COMMENT]))
+        print("Error: syntax error in operand '{}'".format(statement[OPERANDS]))
         sys.exit(1)
 
     return OPERATIONS[operation][mode]
@@ -240,7 +348,7 @@ def translate_operand(statement):
     @rtype: ( int, int )
     '''
     mode = statement[MODE]
-    operand = statement[OPERAND]
+    operand = statement[OPERANDS]
 
     if mode == INH:
         return (0, 0) 
@@ -267,16 +375,40 @@ def translate(statement):
 
 
 def main(args):
-    '''
+    """
     Runs the assembler with the specified arguments.
-    '''
-    lines = read_file(args.filename)
-    for line in lines:
-        result = parse_line(line)
-        if result:
-            translate(result)
-            
 
+    @param args: the arguments to the main function
+    @type: namedtuple
+    """
+    global symbol_table
+    symbol_table = dict()
+    statements = []
+    address = 0x200
+
+    # Pass 1: parse all of the statements in the file, but do not attempt
+    # to resolve any of the labels or locations
+    with open(args.filename) as infile:
+        for line in infile:
+            statement = Statement()
+            statement.parse_line(line)
+            if not statement.is_empty():
+                statements.append(statement)
+
+    # Pass 2: translate the statements into their respective opcodes
+    for index in xrange(len(statements)):
+        statement = statements[index]
+        try:
+            statement.translate()
+        except TranslationError as error:
+            throw_error(error, statement)
+        label = statement.get_label()
+        if label:
+            if label in symbol_table:
+                error = {"value": "label [" + label + "] redefined"}
+                throw_error(error, statement)
+            symbol_table[label] = index
+            
 
 # M A I N #####################################################################
 
